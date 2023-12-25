5fe4c376cbb85424e428f46d3c1599e094c4dbda
Samual Martin <38009832+samualmartin@users.noreply.github.com>
2023-12-25 12:12:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirectToYay() {
+    window.location.href = 'yay.html';
+  }
+  
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth) - 85;
+    var y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight) - 48;
+    document.getElementById('noButton').style.left = `${x}px`;
+    document.getElementById('noButton').style.top = `${y}px`;
+}
+  
+  function redirectToSobSob() {
+    window.location.href = 'sob-sob.html';
+    }
+  
